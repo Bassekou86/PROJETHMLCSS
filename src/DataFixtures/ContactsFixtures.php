1089d56f002999f8a2c79c421e3f833a0da473f7
Bassekou86 <b.kabadiakite86@gmail.com>
2024-02-09 15:22:29 +0100

    gestion contact

--- src/DataFixtures/ContactsFixtures.php
+++ src/DataFixtures/ContactsFixtures.php
@@ -12,23 +12,24 @@ class ContactsFixtures extends Fixture
     {
         $faker  = \Faker\Factory::create('fr_FR');
         $genres = ["male", "female"];
+        for ($i=0 ; $i < 100 ; $i++) {
         $sexe = mt_rand(0,1);
-        if ($sexe == 0) {
+        if ($sexe == 0){
             $type = 'men';
         } elseif($sexe == 1){
             $type = 'women';
         }
-        for ($i=0 ; $i < 100 ; $i++) {
         $contacts = new Contacts();
         $contacts->setNom($faker->lastName())
-                 ->setPrenom($faker->firstName($genres[mt_rand(0,1)]))
-                 ->setRue($faker->streetAddress())
-                 ->setCp($faker->numberBetween(33000,75000))
-                 ->setVille($faker->city())
-                 ->setMail($faker->email())
-                 ->setAvatar("https:://randomuser.me/api/portraits/"."/".$type. $i.".jpg");
-    $manager->persist( $contacts );
+            ->setPrenom($faker->firstName($genres[$sexe]))
+            ->setRue($faker->streetAddress())
+            ->setCp($faker->numberBetween(33000,75000))
+            ->setVille($faker->city())
+            ->setMail($faker->email())
+            ->setSexe ($sexe)
+            ->setAvatar("https:://randomuser.me/api/portraits/".$type ."/". $i.".jpg");
+    $manager->persist($contacts);
         }
-        $manager->flush() ;
+        $manager->flush();
     }
 }
